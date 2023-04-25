d0a12a1ff77476d06660dd2e215511c53effd8df
JisungLee <jskurt9352@gmail.com>
2023-04-25 21:45:57 +0900

    finish up 01 homework

--- src/components/containers/Todo.js
+++ src/components/containers/Todo.js
@@ -4,22 +4,61 @@ import TodoTop from '../presentations/TodoTop'
 
 function Todo() {
   const [todos, setTodos] = useState([])
+  const [edit, setEdit] = useState()
   const addTodo = (todo) => {
     setTodos([...todos, todo])
   }
   const deleteTodo = (id) => {
     setTodos((prev) => prev.filter((todo) => todo.id !== id))
   }
+  const editStateCheck = (id) => {
+    setTodos(todos.map((todo) => (todo.editState = false)))
 
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, editState: !todo.editState } : todo
+      )
+    )
+
+    setEdit(true)
+  }
+
+  const cancelCheck = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, editState: !todo.editState } : todo
+      )
+    )
+
+    setEdit(false)
+  }
+
+  const changeTodo = (id, textItem) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? { ...todo, text: textItem, editState: !todo.editState }
+          : todo
+      )
+    )
+    setEdit(false)
+  }
+  console.log(todos)
   return (
     <div>
-      <TodoTop addTodo={addTodo} />
+      <TodoTop addTodo={addTodo} edit={edit} setEdit={setEdit} />
       {todos.map((todo) => (
         <TodoItem
           key={todo.id}
           id={todo.id}
           text={todo.text}
+          changeTodo={changeTodo}
           deleteTodo={deleteTodo}
+          edit={edit}
+          setEdit={setEdit}
+          editState={todo.editState}
+          editStateCheck={editStateCheck}
+          cancelCheck={cancelCheck}
         />
       ))}
     </div>
