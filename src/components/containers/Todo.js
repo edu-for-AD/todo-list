7ded995b72635ef06bf40167dcdb60b9ea4ed427
jungwoogyun <jwg8910@naver.com>
2023-04-25 17:58:58 +0900

    Success edit button

--- src/components/containers/Todo.js
+++ src/components/containers/Todo.js
@@ -4,21 +4,54 @@ import TodoTop from '../presentations/TodoTop'
 
 function Todo() {
   const [todos, setTodos] = useState([])
+  const [isedit, setIsedit] = useState(false)
+
+  //글추가
   const addTodo = (todo) => {
+    todo.isedit = false
+    todo.isarchive = false
     setTodos([...todos, todo])
   }
+
+  //글삭제
   const deleteTodo = (id) => {
     setTodos((prev) => prev.filter((todo) => todo.id !== id))
   }
+
+  //글수정
+  const editTodo = (todo) => {
+    todos.map((prev) => {
+      if (prev.id === todo.id) {
+        prev.isedit = !prev.isedit
+
+        if (isedit) {
+          prev.text = todo.text
+        }
+        setIsedit(!isedit)
+
+        console.log('editTodo', todo)
+        return
+      }
+    })
+  }
+
+  //휴지통(아카이브)
+  const archiveTodo = (id) => {}
 
   return (
     <div>
       <TodoTop addTodo={addTodo} />
       {todos.map((todo) => (
         <TodoItem
+          // key={todo.id}
+          // id={todo.id}
+          // text={todo.text}
+          // isedit={todo.isedit}
+          // isachive={todo.isachive}
           key={todo.id}
-          id={todo.id}
-          text={todo.text}
+          todo={todo}
+          editTodo={editTodo}
+          archiveTodo={archiveTodo}
           deleteTodo={deleteTodo}
         />
       ))}
