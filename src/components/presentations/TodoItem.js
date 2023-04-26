a64a0e68eed5aa142913f5ca8f4f2bb192ad39b3
JisungLee <jskurt9352@gmail.com>
2023-04-26 16:01:35 +0900

    add archive button

--- src/components/presentations/TodoItem.js
+++ src/components/presentations/TodoItem.js
@@ -1,13 +1,28 @@
+import { useState } from 'react'
+
 function TodoItem({ id, text, deleteTodo }) {
+  const [archive, setArchive] = useState(false)
+  const handleOnClickArchive = () => {
+    setArchive(true)
+  }
   return (
     <div style={{ display: 'flex', width: '100%' }}>
       <div>{text}</div>
       <button
+        style={archive ? null : { display: 'none' }}
         onClick={() => {
           deleteTodo(id)
         }}
       >
         delete
+      </button>
+      <button style={archive ? { display: 'none' } : null}>edit</button>
+
+      <button
+        style={archive ? { display: 'none' } : null}
+        onClick={handleOnClickArchive}
+      >
+        archive
       </button>
     </div>
   )
