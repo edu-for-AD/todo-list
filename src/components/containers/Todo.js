ef3fa25fd8525e2f2364a2b68ee01634e5dba263
Jisung Lee <jisunglee@Jisungs-Air.lan>
2023-06-21 13:16:42 -0400

    add useCustomHook
    1. modify Todo.js
    2. add useTodo.js

--- src/components/containers/Todo.js
+++ src/components/containers/Todo.js
@@ -3,15 +3,23 @@ import TodoItem from '../presentations/TodoItem'
 import TodoTop from '../presentations/TodoTop'
 import TodoFilter from '../presentations/TodoFilter'
 import { useSearchParams } from 'react-router-dom'
+import { useTodo } from '../containers/useTodo'
 
 function Todo() {
-  const [todos, setTodos] = useState([])
+  const {
+    todos,
+    addTodo,
+    deleteTodo,
+    changeEditingStatus,
+    toggleArchiveStatus,
+    toggleCompleteStatus,
+    cancelTodo,
+    confirmTodo
+  } = useTodo()
   const [searchParams, setSearchParams] = useSearchParams()
   const [filterArchive, setFilterArchive] = useState('all')
   const [filterComplete, setFilterComplete] = useState('all')
-
   const editing = todos.some((todo) => todo.editing === true)
-
   const filterTodos = todos
     .filter((todo) => {
       if (filterArchive === 'archived') {
@@ -34,113 +42,6 @@ function Todo() {
       return todos
     })
 
-  const addTodo = (todo) => {
-    fetch(`${process.env.REACT_APP_BASEURL}/todos`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ title: todo.text })
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        setTodos([...todos, result])
-      })
-  }
-
-  const deleteTodo = (id) => {
-    fetch(`${process.env.REACT_APP_BASEURL}/todos/${id}`, {
-      method: 'DELETE'
-    })
-      .then((response) => response.json())
-      .then(() => {
-        setTodos((prev) => prev.filter((todo) => todo.id !== id))
-      })
-  }
-
-  const changeEditingStatus = (id) => {
-    setTodos((todos) =>
-      todos.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, editing: !todo.editing }
-        }
-
-        if (todo.editing === true) {
-          return { ...todo, editing: false }
-        }
-
-        return todo
-      })
-    )
-  }
-
-  const toggleArchiveStatus = (id, archived) => {
-    fetch(`${process.env.REACT_APP_BASEURL}/todos/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ archived: !archived })
-    })
-      .then((response) => response.json())
-      .then((updatedTodo) => {
-        setTodos((todos) =>
-          todos.map((todo) => {
-            if (todo.id === id) {
-              return updatedTodo
-            }
-            return todo
-          })
-        )
-      })
-  }
-
-  const toggleCompleteStatus = (id, completed) => {
-    fetch(`${process.env.REACT_APP_BASEURL}/todos/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ completed: !completed })
-    })
-      .then((response) => response.json())
-      .then((updatedTodo) => {
-        setTodos((todos) =>
-          todos.map((todo) => {
-            if (todo.id === id) {
-              return updatedTodo
-            }
-            return todo
-          })
-        )
-      })
-  }
-  const cancelTodo = (id) => {
-    setTodos((todos) =>
-      todos.map((todo) => (todo.id === id ? { ...todo, editing: false } : todo))
-    )
-  }
-
-  const confirmTodo = (id, textItem) => {
-    fetch(`${process.env.REACT_APP_BASEURL}/todos/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ title: textItem })
-    })
-      .then((response) => response.json())
-      .then((updatedTodo) => {
-        setTodos((todos) =>
-          todos.map((todo) => {
-            if (todo.id === id) {
-              return updatedTodo
-            }
-            return todo
-          })
-        )
-      })
-  }
   useEffect(() => {
     const archiveParams = searchParams.get('filterArchiveStatus')
     const completeParams = searchParams.get('filterCompleteStatus')
@@ -172,14 +73,6 @@ function Todo() {
       setFilterComplete(completeParams)
     }
   }, [searchParams, setSearchParams])
-
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_BASEURL}/todos`)
-      .then((response) => response.json())
-      .then((result) => {
-        setTodos(result)
-      })
-  }, [])
 
   return (
     <div>
