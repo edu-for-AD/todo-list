7532d53f5efc4f8087c4912a51366ecea2aa0cfc
Jisung Lee <jisunglee@Jisungs-Air.lan>
2023-06-22 13:03:51 -0400

    convert hook files to ts

---
+++ src/components/containers/useTodoFilter.ts
@@ -0,0 +1,92 @@
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+import { Todo } from '../../types/todo'
+
+enum ArchiveTypes {
+  ALL = 'all',
+  ARCHIVED = 'archived',
+  UNARCHIVED = 'unarchived'
+}
+
+enum CompleteTypes {
+  ALL = 'all',
+  COMPLETED = 'completed',
+  UNCOMPLETED = 'uncompleted'
+}
+
+export const useTodoFilter = (todos: Todo[]) => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [filterArchive, setFilterArchive] = useState<ArchiveTypes>(
+    ArchiveTypes.ALL
+  )
+  const [filterComplete, setFilterComplete] = useState<CompleteTypes>(
+    CompleteTypes.ALL
+  )
+  const editing = todos.some((todo) => todo.editing === true)
+  const filterTodos = todos
+    .filter((todo) => {
+      if (filterArchive === ArchiveTypes.ARCHIVED) {
+        return todo.archived
+      }
+
+      if (filterArchive === ArchiveTypes.UNARCHIVED) {
+        return !todo.archived
+      }
+      return todos
+    })
+    .filter((todo) => {
+      if (filterComplete === CompleteTypes.COMPLETED) {
+        return todo.completed
+      }
+
+      if (filterComplete === CompleteTypes.UNCOMPLETED) {
+        return !todo.completed
+      }
+      return todos
+    })
+
+  useEffect(() => {
+    const archiveParams: unknown = searchParams.get('filterArchiveStatus')
+    const completeParams: unknown = searchParams.get('filterCompleteStatus')
+
+    if (
+      archiveParams === null ||
+      (archiveParams !== ArchiveTypes.ALL &&
+        archiveParams !== ArchiveTypes.ARCHIVED &&
+        archiveParams !== ArchiveTypes.UNARCHIVED)
+    ) {
+      setSearchParams({
+        filterArchiveStatus: ArchiveTypes.ALL,
+        filterCompleteStatus: CompleteTypes.ALL
+      })
+    } else {
+      setFilterArchive(archiveParams)
+    }
+
+    if (
+      completeParams === null ||
+      (completeParams !== CompleteTypes.ALL &&
+        completeParams !== CompleteTypes.COMPLETED &&
+        completeParams !== CompleteTypes.UNCOMPLETED)
+    ) {
+      setSearchParams({
+        filterArchiveStatus: ArchiveTypes.ALL,
+        filterCompleteStatus: CompleteTypes.ALL
+      })
+    } else {
+      setFilterComplete(completeParams)
+    }
+  }, [searchParams, setSearchParams])
+
+  return {
+    searchParams,
+    filterArchive,
+    filterComplete,
+    editing,
+    filterTodos,
+    setFilterArchive,
+    setFilterComplete,
+    setSearchParams
+  }
+}
